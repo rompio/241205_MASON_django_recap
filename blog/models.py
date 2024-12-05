386ec3451011b0a1e0c202537ef049f5198ea179
ramon <ramonpolanski@gmail.com>
2024-12-05 14:29:03 +0100

    integrated Custom User Model and reanranged migrations and db

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)  
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)  
+    content = models.TextField() 
+    created_at = models.DateTimeField(auto_now_add=True)  
+    updated_at = models.DateTimeField(auto_now=True)  
+    categories = models.ManyToManyField(Category, related_name='posts')  
+    author = models.ForeignKey(User, on_delete=models.CASCADE)  
+
+    def __str__(self):
+        return self.title
